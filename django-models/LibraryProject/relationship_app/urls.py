a5bcd4dd09db529f012f6729518b0814e9f9c4a8
wilson <wilsonedet96@gmail.com>
2025-09-07 22:29:37 +0100

    user_auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name = 'library_detail')
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name = 'library_detail'),
+    path('register/', register, name = 'register'),
+    path('login/', CustomLoginView.as_view(), name = 'login'),
+    path('logout/', CustomLogoutView.as_view(), name = 'logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -13,3 +16,20 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register,html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
